36948283a9af43ce8dd5e58795d6295560fa1d77
Hussain Lohawala <73359258+H9660@users.noreply.github.com>
2025-03-24 19:19:59 +0530

    Fixed cors errors

--- index.ts
+++ index.ts
@@ -6,27 +6,8 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import cors from "cors";
 const app = express();
 dotenv.config()
-const allowedOrigins = [
-  process.env.FRONTEND_URL,  // Stable frontend URL
-  /^https:\/\/your-frontend-domain.*\.vercel\.app$/ // Dynamic preview URLs
-];
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = req.get('origin')??"";
-
-  if (origin && allowedOrigins.some((o) => 
-    typeof o === 'string' ? o === origin : o.test(origin)
-  )) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
-
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-    return;
-  }
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 app.use(express.json());
