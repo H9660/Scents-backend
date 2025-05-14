543946474ede79bef1c4ffe10a4293917ea71683
Hussain Lohawala <73359258+H9660@users.noreply.github.com>
2025-05-14 10:59:47 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,35 +6,11 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import cors from "cors";
 const app = express();
 dotenv.config()
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  'https://frontend-chi-seven-83.vercel.app'
-];
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const origin = req.get('origin') ?? '';
-
-  if (origin && allowedOrigins.some((o) => 
-    typeof o === 'string' ? o === origin : o.test(origin)
-  )) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
-
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-    return;
-  }
-
-  next();
-});
-
+app.use(
+  cors({
+    origin: ["https://frontend-chi-seven-83.vercel.app", process.env.FRONTEND_URL],
+  })
+);
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/perfume", perfumeRoutes);
